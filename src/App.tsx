8c4a70f21d71e9ec6ffc4f59c4fed67d69a4c648
DevelopmentGhost123 <oniks0261@gmail.com>
2024-05-16 02:19:43 -0700

    global.ts Done

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,12 @@
-import React from 'react';
+import React from "react";
+import { GlobalStyle } from "./styles/global";
 
 function App() {
   return (
-      <></>
-    );
+    <>
+      <GlobalStyle />
+    </>
+  );
 }
 
 export default App;
