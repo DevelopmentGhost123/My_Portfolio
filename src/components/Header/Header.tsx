1f2161c55be6ecea6e9b5a8d6df7b5cb42ffffa1
DevelopmentGhost123 <oniks0261@gmail.com>
2024-05-16 08:00:42 -0700

    assets edit

---
+++ src/components/Header/Header.tsx
@@ -0,0 +1,6 @@
+import { Container } from "./style";
+import { BrowserRouter as Router } from "react-router-dom";
+import { NavHashLink, HashLink } from "react-router-hash-link";
+import { useState } from "react";
+
+export function Header() {}
---
+++ src/components/Header/style.ts
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const Container = styled.header`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1.8rem 10rem;
+
+  background-color: #21212150;
+
+  backdrop-filter: blur(6px);
+`;
