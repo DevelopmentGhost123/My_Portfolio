0c2ea5a53004cc8602aac291dfb2303e886cf684
DevelopmentGhost123 <oniks0261@gmail.com>
2024-05-19 22:08:17 -0700

    Portfolio Edit

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { GlobalStyle } from "./styles/global";
+import { Header } from "./components/Header/Header";
+import { Main } from "./components/Main/Main";
+import { Footer } from "./components/Footer/Footer";
 
 function App() {
   return (
     <>
       <GlobalStyle />
+      <Header />
+      <Main />
+      <Footer />
     </>
   );
 }
